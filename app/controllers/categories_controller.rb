7e2eb8c8944b7835d7b86384b31c52a255f56e49
Nestor Raul <raul260290@gmail.com>
2012-11-16 15:12:05 -0700

    Cambio de redireccionamiento al editar una categoria

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -78,7 +78,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.update_attributes(params[:category])
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to categories_path, notice: 'Categoria Actualizada Correctamente' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
